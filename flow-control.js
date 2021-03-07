5d0fea9468a1a2cbbe5496eef66a98eff839e23f
Shelby Talbert <mi.shelbyrose@gmail.com>
2021-03-07 00:37:24 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -4,10 +4,12 @@ if(age >= 13 && age <= 19) return "You are a teenager!"
 
 function teenager(age) {
 if(age >= 13 && age <= 19) return "You are a teenager!"
-else return "You are not a teenager"
+return "You are not a teenager"
 }
 
 function ageChecker(age) {
+  if(age >= 13 && age <= 19) return "You are a teenager!"
+  return "You are not a teenager"
 
 }
 
